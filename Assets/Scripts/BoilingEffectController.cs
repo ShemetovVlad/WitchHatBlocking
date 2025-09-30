661038cd9e29d033c753af433b8f82f9cd446546
ShemetovVlad <shemetov.vn@yandex.ru>
2025-10-01 00:34:59 +0300

    Cauldron effects, cauldron UI disable after 3 ingredients

---
+++ Assets/Scripts/BoilingEffectController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class BoilingEffectController : MonoBehaviour
+{
+    [SerializeField] private CauldronCounter cauldronCounter;
+    [SerializeField] private ParticleSystem boilingParticles;
+
+    private void Start()
+    {
+        if (boilingParticles == null)
+            boilingParticles = GetComponent<ParticleSystem>();
+
+        cauldronCounter.OnIngredientAdded += OnIngredientAdded;
+        cauldronCounter.OnCauldronCleared += OnCauldronCleared;
+
+        // ON boiling at start
+        if (boilingParticles != null && !boilingParticles.isPlaying)
+        {
+            boilingParticles.Play();
+        }
+    }
+
+    private void OnIngredientAdded(object sender, KitchenObjectSO ingredientSO)
+    {
+        if (boilingParticles != null && boilingParticles.isPlaying)
+        {
+            boilingParticles.Stop();
+        }
+    }
+
+    private void OnCauldronCleared(object sender, System.EventArgs e)
+    {
+        if (boilingParticles != null && !boilingParticles.isPlaying)
+        {
+            boilingParticles.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/StirringAnimationController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class StirringAnimationController : MonoBehaviour
+{
+    [SerializeField] private CauldronCounter cauldronCounter;
+    private Animation stirringAnimation;
+
+    private void Start()
+    {
+        stirringAnimation = GetComponent<Animation>();
+        stirringAnimation.Stop();
+        cauldronCounter.OnIngredientAdded += OnIngredientAdded;
+        cauldronCounter.OnCauldronCleared += OnCauldronCleared;
+    }
+
+    private void OnIngredientAdded(object sender, KitchenObjectSO ingredientSO)
+    {
+        if (stirringAnimation != null)
+        {
+            stirringAnimation.Play();
+        }
+    }
+
+    private void OnCauldronCleared(object sender, System.EventArgs e)
+    {
+        if (stirringAnimation != null)
+        {
+            stirringAnimation.Stop();
+        }
+    }
+}

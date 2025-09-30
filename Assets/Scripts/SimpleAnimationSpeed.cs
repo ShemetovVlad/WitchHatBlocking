9a0562c77358cc127964a90893fb415a45dbd95a
ShemetovVlad <shemetov.vn@yandex.ru>
2025-09-30 14:50:09 +0300

    Cauldron counter + simple animation speed changer

---
+++ Assets/Scripts/SimpleAnimationSpeed.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class SimpleAnimationSpeed : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    void Start()
+    {
+        Animation animation = GetComponent<Animation>();
+        if (animation == null) return;
+
+        foreach (AnimationState state in animation)
+        {
+            state.speed = speed;
+            break;
+        }
+    }
+}

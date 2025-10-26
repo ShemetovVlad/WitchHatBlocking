bafdd9788667274bf3585e5c2027f76c0b55e818
ShemetovVlad <shemetov.vn@yandex.ru>
2025-10-26 12:58:08 +0300

    Rewarded adv to Yandex SDK

---
+++ Assets/Scripts/GameData/RewardedAdvYG.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using YG;
+
+public class RewardedAdvYG : MonoBehaviour
+{
+    [SerializeField] private int coinsReward;
+    public string idAdv;
+
+    private void OnEnable()
+    {
+        YG2.onRewardAdv += OnRewarded;
+    }
+
+    private void OnDisable()
+    {
+        YG2.onRewardAdv -= OnRewarded;
+    }
+
+    private void OnRewarded(string id)
+    {
+        if (id == idAdv)
+        {
+            SetReward();
+        }
+    }
+
+    public void SetReward()
+    {
+        Debug.Log($"Награда получена! +{coinsReward} монет");
+        PlayerWallet.Instance.AddMoney(coinsReward);
+    }
+}

b4eeb1f66e0d656ef717b1ae0271506eda812ccf
ShemetovVlad <shemetov.vn@yandex.ru>
2025-10-29 18:51:06 +0300

    Language changer YG

---
+++ Assets/Scripts/GameData/LanguageChangerYG.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace YG.Example
+{
+    public class LanguageChanger : MonoBehaviour
+    {
+        public string ru, en;
+
+        private Text textComponent;
+
+        private void Awake()
+        {
+            textComponent = GetComponent<Text>();
+        }
+
+        private void OnEnable()
+        {
+            YG2.onSwitchLang += SwitchLanguage;
+            SwitchLanguage(YG2.lang);
+        }
+        private void OnDisable()
+        {
+            YG2.onSwitchLang -= SwitchLanguage;
+        }
+
+        public void SwitchLanguage(string lang)
+        {
+            switch (lang)
+            {
+                case "ru":
+                    textComponent.text = ru;
+                    break;
+                default:
+                    textComponent.text = en;
+                    break;
+            }
+        }
+    }
+}
